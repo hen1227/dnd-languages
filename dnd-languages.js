ea9d84b97afd87238fb388ca1e89c79b23c78dd0
BuildTools <unconfigured@null.spigotmc.org>
2022-07-25 17:26:45 -0400

    Fixed Directroy issues for image uploads

--- dnd-languages.js
+++ dnd-languages.js
@@ -9,18 +9,18 @@ module.exports = {
 
     upload: function upload(req, res) {
         let realJson = JSON.parse(Object.keys(req.body)[0] +Object.values(req.body)[0]);
-        const files = fs.readdirSync('/server/dnd-languages/database/'+realJson.language+'/'+realJson.letter+'/')
+        const files = fs.readdirSync('database/'+realJson.language+'/'+realJson.letter+'/')
         var letterExt = files.length + 1;
-        decodeBase64Image('/server/dnd-languages/database/'+realJson.language+'/'+realJson.letter+'/Letter'+letterExt.toString()+".jpg", realJson.image.replace(/\ /g, '+'));
+        decodeBase64Image('database/'+realJson.language+'/'+realJson.letter+'/Letter'+letterExt.toString()+".jpg", realJson.image.replace(/\ /g, '+'));
         
         res.status(200).json({"Status": "Uploaded Successful!", "Message": "The "+letterExt.toString()+"th "+realJson.letter+" has been uploaded to the database" });
     },
     getCount: function getCount(req, res) {
         console.log(req.body)
         var response = []
-        const dir = fs.readdirSync('/server/dnd-languages/database/'+req.body.language+'/')
+        const dir = fs.readdirSync('database/'+req.body.language+'/')
         for (const symbol of dir){
-            const len = fs.readdirSync('/server/dnd-languages/database/'+req.body.language+'/'+symbol).length
+            const len = fs.readdirSync('database/'+req.body.language+'/'+symbol).length
             response.push(len)
         }
         res.status(200).json({"Symbols":dir,"Numbers":response});
